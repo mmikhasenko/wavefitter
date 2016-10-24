21f91daa0c2abdf41e09dada37302d1781859832
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2016-10-24 17:48:32 -0400

    isobar shape rewrite

--- kmatrix/test_isobar_shape.cc
+++ kmatrix/test_isobar_shape.cc
@@ -1,6 +1,5 @@
 // Copyright [2016] Mikhail Mikhasenko
 
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -97,94 +96,92 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-=======
-#include "TH2D.h"
-#include "TGraph.h"
-#include "TCanvas.h"
-#include "MIsobar.h"
-#include "MIsobarChannel.h"
-#include "MIsobarPiPiS.h"
-#include "MTwoBodyChannel.h"
-#include "mstructures.h"
-
-int main(int argc, char *argv[]) {
-  
-  MIsobarPiPiS  pipiS_iso;
-  MIsobar rho_iso(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.);
-  MIsobar  f2_iso(F2_MASS, F2_WIDTH,  PI_MASS, PI_MASS, 2, 5.);
-
-  TCanvas c1("c1");
-  TMultiGraph *m;  
-  /* RHO */
-  m = combine(
-          SET3(
-               draw([&](double m)->double{return real(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Real part"),
-               SetFillColor(0),
-               SetLineColor(kBlue)),
-          SET3(
-               draw([&](double m)->double{return imag(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Imag part"),
-               SetFillColor(0),
-               SetLineColor(kOrange) ),
-          SET3(
-               draw([&](double m)->double{return abs(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Abs part"),
-               SetFillColor(0),
-               SetLineColor(kGreen) ) );
-  m->SetTitle("Amplitude pipiS(s)");
-  m->Draw("al");
-  c1.BuildLegend();
-  c1.SaveAs("/tmp/test_isobar_shale.pdf(");
-  c1.Clear();
-
-  /* RHO */
-  m = combine(
-          SET3(
-               draw([&](double m)->double{return real(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Real part"),
-               SetFillColor(0),
-               SetLineColor(kBlue)),
-          SET3(
-               draw([&](double m)->double{return imag(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Imag part"),
-               SetFillColor(0),
-               SetLineColor(kOrange) ),
-          SET3(
-               draw([&](double m)->double{return abs(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Abs part"),
-               SetFillColor(0),
-               SetLineColor(kGreen) ) );
-  m->SetTitle("Amplitude rho(s)");
-  m->Draw("al");
-  c1.BuildLegend();
-  c1.SaveAs("/tmp/test_isobar_shale.pdf");
-  c1.Clear();
-
-  /* F2 */
-  m = combine(
-          SET3(
-               draw([&](double m)->double{return real(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Real part"),
-               SetFillColor(0),
-               SetLineColor(kBlue)),
-          SET3(
-               draw([&](double m)->double{return imag(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Imag part"),
-               SetFillColor(0),
-               SetLineColor(kOrange) ),
-          SET3(
-               draw([&](double m)->double{return abs(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
-               SetTitle("Abs part"),
-               SetFillColor(0),
-               SetLineColor(kGreen) ) );
-  m->SetTitle("Amplitude f2(s)");
-  m->Draw("al");
-  c1.BuildLegend();
-  c1.SaveAs("/tmp/test_isobar_shale.pdf)");
-  c1.Clear();
-
-  return 0;
-}
-
->>>>>>> 8de601b5d095dd6d24c887cdca8a7d6c05654329
+// =======
+// #include "TH2D.h"
+// #include "TGraph.h"
+// #include "TCanvas.h"
+// #include "MIsobar.h"
+// #include "MIsobarChannel.h"
+// #include "MIsobarPiPiS.h"
+// #include "MTwoBodyChannel.h"
+// #include "mstructures.h"
+// 
+// int main(int argc, char *argv[]) {
+//   
+//   MIsobarPiPiS  pipiS_iso;
+//   MIsobar rho_iso(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.);
+//   MIsobar  f2_iso(F2_MASS, F2_WIDTH,  PI_MASS, PI_MASS, 2, 5.);
+// 
+//   TCanvas c1("c1");
+//   TMultiGraph *m;  
+//   /* RHO */
+//   m = combine(
+//           SET3(
+//                draw([&](double m)->double{return real(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Real part"),
+//                SetFillColor(0),
+//                SetLineColor(kBlue)),
+//           SET3(
+//                draw([&](double m)->double{return imag(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Imag part"),
+//                SetFillColor(0),
+//                SetLineColor(kOrange) ),
+//           SET3(
+//                draw([&](double m)->double{return abs(pipiS_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Abs part"),
+//                SetFillColor(0),
+//                SetLineColor(kGreen) ) );
+//   m->SetTitle("Amplitude pipiS(s)");
+//   m->Draw("al");
+//   c1.BuildLegend();
+//   c1.SaveAs("/tmp/test_isobar_shale.pdf(");
+//   c1.Clear();
+// 
+//   /* RHO */
+//   m = combine(
+//           SET3(
+//                draw([&](double m)->double{return real(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Real part"),
+//                SetFillColor(0),
+//                SetLineColor(kBlue)),
+//           SET3(
+//                draw([&](double m)->double{return imag(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Imag part"),
+//                SetFillColor(0),
+//                SetLineColor(kOrange) ),
+//           SET3(
+//                draw([&](double m)->double{return abs(rho_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Abs part"),
+//                SetFillColor(0),
+//                SetLineColor(kGreen) ) );
+//   m->SetTitle("Amplitude rho(s)");
+//   m->Draw("al");
+//   c1.BuildLegend();
+//   c1.SaveAs("/tmp/test_isobar_shale.pdf");
+//   c1.Clear();
+// 
+//   /* F2 */
+//   m = combine(
+//           SET3(
+//                draw([&](double m)->double{return real(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Real part"),
+//                SetFillColor(0),
+//                SetLineColor(kBlue)),
+//           SET3(
+//                draw([&](double m)->double{return imag(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Imag part"),
+//                SetFillColor(0),
+//                SetLineColor(kOrange) ),
+//           SET3(
+//                draw([&](double m)->double{return abs(f2_iso.T(m*m));}, 2*PI_MASS, 2.2),
+//                SetTitle("Abs part"),
+//                SetFillColor(0),
+//                SetLineColor(kGreen) ) );
+//   m->SetTitle("Amplitude f2(s)");
+//   m->Draw("al");
+//   c1.BuildLegend();
+//   c1.SaveAs("/tmp/test_isobar_shale.pdf)");
+//   c1.Clear();
+// 
+//   return 0;
+// }
