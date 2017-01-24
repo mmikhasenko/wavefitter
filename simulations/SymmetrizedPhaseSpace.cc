87ec6defe69a202c0d3bfca238a3c452d54d6450
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-01-24 17:35:52 +0100

    Add calculation of phase space integral

--- simulations/SymmetrizedPhaseSpace.cc
+++ simulations/SymmetrizedPhaseSpace.cc
@@ -3,24 +3,33 @@
 
 #include "MIsobar.h"
 #include "M3bodyAngularBasis.h"
+
 #include "constants.h"
+#include "mintegrate.h"
 #include "deflib.h"
 
 
 int main() {
-
+  // set parameters
   MIsobar rho(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.); rho.setIntU();
   uint J = 4, M = 0, L = 3, S = 1;
+  uint NintPoints = 10000;
 
-  uint Npoints = 11;
+  uint Npoints = 21;
   std::pair<double, double> range(0.5, 2.5);
   for (uint i = 0; i < Npoints; i++) {
     double s = POW2(range.first + (range.second-range.first)/(Npoints-1)*i);
-    std::cout << "sqrt(s), Symm, Non-Symm: " << sqrt(s) << "  ";
-      /***********************************************************************************/
-      // Symmetrized
+    /***********************************************************************************/
+    // Phhase space
+    double phsp = integrate([s](double s1)->double{
+        return sqrt(LAMBDA(s, s1, POW2(PI_MASS))*LAMBDA(s1, POW2(PI_MASS), POW2(PI_MASS)))/s1;
+      }, 4*POW2(PI_MASS), POW2(sqrt(s)-PI_MASS)) / (2*M_PI*POW2(8*M_PI)*s);
+
+    std::cout << "sqrt(s), Ph.Sp., Symm, Non-Symm: " << sqrt(s) << "  " << phsp << "  ";
+    /***********************************************************************************/
+    // Symmetrized
     std::function<double(double, double, double, double, double,
-                         double, double, double, double, double)> 
+                         double, double, double, double, double)>
       Msq_symm = [&, J, M, L, S](double s1, double theta1, double phi1, double theta23, double phi23,
                                  double s3, double theta3, double phi3, double theta12, double phi12)->double{
       cd Zf1 = Math::ZJMLS(J, M, L, S, theta1, phi1, theta23, phi23);
@@ -28,7 +37,7 @@ int main() {
       // cd amp = Zf3;
       cd amp = (rho.T(s1)*Zf1 + rho.T(s3)*Zf3) / sqrt(2.);
       return norm(amp);};
-    double int_symm = Math::integrate3bphs(Msq_symm, 10000, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
+    double int_symm = Math::integrate3bphs(Msq_symm, NintPoints, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
     std::cout << int_symm << "  ";
 
     /***********************************************************************************/
@@ -36,12 +45,12 @@ int main() {
     std::function<double(double, double, double, double, double,
                          double, double, double, double, double)>
       Msq_nonsymm = [&, J, M, L, S](double s1, double theta1, double phi1, double theta23, double phi23,
-                                    double s3, double theta3, double phi3, double theta12, double phi12)->double{                   
+                                    double s3, double theta3, double phi3, double theta12, double phi12)->double{
       cd Zf1 = Math::ZJMLS(J, M, L, S, theta1, phi1, theta23, phi23);
       // cd amp = Zf3;
       cd amp = rho.T(s1)*Zf1;
       return norm(amp);};
-    double int_nonsymm = Math::integrate3bphs(Msq_nonsymm, 10000, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
+    double int_nonsymm = Math::integrate3bphs(Msq_nonsymm, NintPoints, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
     std::cout << int_nonsymm << "\n";
   }
 
