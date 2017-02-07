d8efb5714abd527c07d2722f67e3ea6bd5684250
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-02-07 10:13:32 +0100

    changed SymmetrizedPhaseSpace to support any isobar

--- simulations/SymmetrizedPhaseSpace.cc
+++ simulations/SymmetrizedPhaseSpace.cc
@@ -2,6 +2,7 @@
 
 
 #include "MIsobar.h"
+#include "MIsobarPiPiS.h"
 #include "M3bodyAngularBasis.h"
 
 #include "constants.h"
@@ -11,11 +12,14 @@
 
 int main() {
   // set parameters
-  MIsobar rho(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.); rho.setIntU();
+  // MIsobar rho(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.); rho.setIntU();
+  MIsobarPiPiS pipiS;  // pipiS.setIntU();
+  MIsobar *iso = &pipiS;
+
   uint J = 4, M = 0, L = 3, S = 1;
-  uint NintPoints = 10000;
 
   uint Npoints = 21;
+  uint NintPoints = 10000;
   std::pair<double, double> range(0.5, 2.5);
   for (uint i = 0; i < Npoints; i++) {
     double s = POW2(range.first + (range.second-range.first)/(Npoints-1)*i);
@@ -35,7 +39,7 @@ int main() {
       cd Zf1 = Math::ZJMLS(J, M, L, S, theta1, phi1, theta23, phi23);
       cd Zf3 = Math::ZJMLS(J, M, L, S, theta3, phi3, theta12, phi12);
       // cd amp = Zf3;
-      cd amp = (rho.T(s1)*Zf1 + rho.T(s3)*Zf3) / sqrt(2.);
+      cd amp = (iso->T(s1)*Zf1 + iso->T(s3)*Zf3) / sqrt(2.);
       return norm(amp);};
     double int_symm = Math::integrate3bphs(Msq_symm, NintPoints, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
     std::cout << int_symm << "  ";
@@ -48,7 +52,7 @@ int main() {
                                     double s3, double theta3, double phi3, double theta12, double phi12)->double{
       cd Zf1 = Math::ZJMLS(J, M, L, S, theta1, phi1, theta23, phi23);
       // cd amp = Zf3;
-      cd amp = rho.T(s1)*Zf1;
+      cd amp = iso->T(s1)*Zf1;
       return norm(amp);};
     double int_nonsymm = Math::integrate3bphs(Msq_nonsymm, NintPoints, s, POW2(PI_MASS), POW2(PI_MASS), POW2(PI_MASS));
     std::cout << int_nonsymm << "\n";
