78292be69dd88e182b9782cd407dc769268b3d4e
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-04-20 15:42:55 +0200

    wrote a program to generate three-body phase-space angles. Integrals in calculate_phase_space are calculated now based on this dataset. Wavepull is read from the file.

--- simulations/calculate_phase_space.cc
+++ simulations/calculate_phase_space.cc
@@ -1,5 +1,7 @@
 // Copyright [2016] Misha Mikhasenko
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <complex>
 
 #include "TLorentzVector.h"
@@ -8,119 +10,163 @@
 #include "TDirectory.h"
 #include "TH1D.h"
 #include "TCanvas.h"
-#include "Math/SpecFuncMathMore.h"
 
 #include "MDeck.h"
 #include "MIsobar.h"
 #include "MIsobarPiPiS.h"
 #include "waves.h"
+#include "mintegrate.h"
+#include "M3bodyAngularBasis.h"
 
 typedef struct {
+  uint index;
   uint J;
   uint M;
-  uint S;
+  bool neg_refl;
+  int S;
   uint L;
   double threshold;
   std::string title;
 } wave;
 
-wave make_wave(uint _J, uint _M, uint _S, uint _L, double _threshold,
+wave make_wave(uint _J, uint _M, uint _S, uint _L, bool _neg_refl = true, double _threshold = 0.5,
                const std::string &_title = "") {
-  wave n; n.J = _J; n.M = _M; n.S = _S; n.L = _L; n.title = _title; n.threshold = _threshold;
+  wave n; n.J = _J; n.M = _M; n.S = _S; n.L = _L;
+  n.neg_refl = _neg_refl; n.title = _title; n.threshold = _threshold;
   return n;
 }
 
+void fill_wavepull(const char* wave_fname, std::vector<wave> *waves);
+
 int main(int argc, char *argv[]) {
 
-  std::vector<wave> waves;
 
-  TH1D htest("text", "phase space 2^{++}0^{+}#rho#pi D-wave", 100, 0.5, 2.5);
-  waves.push_back(make_wave(2, 0, 2, 2, 1.0, "2^{++}0^{+}#rho#pi D-wave"));
-  const double iw = 0;  // wave index waves[iw];
+  TH1D htest("test", "nonSymm. phase space 2^{++}0^{+}#rho#pi D-wave", 100, 0.5, 2.5);
+  TH1D htest2("test2", "Symm. phase space 2^{++}0^{+}#rho#pi D-wave", 100, 0.5, 2.5);
+  // waves.push_back(make_wave(2, 0, 2, 2, 1.0, "2^{++}0^{+}#rho#pi D-wave"));
+  std::vector<wave> waves;
+  fill_wavepull("/localhome/mikhasenko/results/pwa_3pi/wavelist_formated.txt", &waves);
+  std::cout << "waves.size() = " << waves.size() << "\n";
+  for (auto & w : waves)
+    std::cout << w.index << ": " << w.title << " " << w.J << " " << w.M << " " << w.S << " " << w.L << "\n";
+  // waves.push_back(make_wave(1, 0, 1, 0, 0.0, "2^{++}0^{+}#rho#pi D-wave"));
+  const double iw = 1;  // wave index waves[iw];
+
+  return 0;
+
+  // for out mode;
+  MIsobar rho_iso(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.); rho_iso.setIntU();
+  MIsobar  f2_iso(F2_MASS, F2_WIDTH,  PI_MASS, PI_MASS, 2, 5.); f2_iso.setIntU();
+  MIsobar *iso[] = {&rho_iso, &rho_iso, &f2_iso};
 
   for (uint e = 0; e < 100; e++) {
+    std::cout << "---> File #" << e << "\n";
     TString fin_name = TString::Format("/mnt/data/compass/2008/phase_space_MC/%d.root", e);
 
     // open file and check
     TFile *f = TFile::Open(fin_name);
     if (!f) {std::cout << "Error: no file" << std::endl; return 0;}
-    TTree *tin = 0; gDirectory->GetObject("events", tin);
+    TTree *tin = 0; gDirectory->GetObject("angles", tin);
     if (!tin) {std::cout << "Error: no tree" << std::endl; return 0;}
 
-    TLorentzVector *beam_lv    = new TLorentzVector();
-    TLorentzVector *pi1_lv     = new TLorentzVector();
-    TLorentzVector *pi2_lv     = new TLorentzVector();
-    TLorentzVector *pi3_lv     = new TLorentzVector();
-    TLorentzVector *recl_lv    = new TLorentzVector();
-    TLorentzVector *reso_lv     = new TLorentzVector();
+    double s;
+    tin->SetBranchAddress("s", &s);
 
-    tin->SetBranchAddress("beam", &beam_lv);
-    tin->SetBranchAddress("ipion1", &pi1_lv);
-    tin->SetBranchAddress("ipion2", &pi2_lv);
-    tin->SetBranchAddress("bpion",  &pi3_lv);
-    tin->SetBranchAddress("3pi", &reso_lv);
-    tin->SetBranchAddress("recoil", &recl_lv);
+    // (23)-frame
+    double s1, costheta1, phi1, costheta23, phi23;
+    tin->SetBranchAddress("s1", &s1);
+    tin->SetBranchAddress("costheta1", &costheta1);
+    tin->SetBranchAddress("phi1", &phi1);
+    tin->SetBranchAddress("costheta23", &costheta23);
+    tin->SetBranchAddress("phi23", &phi23);
 
-    double mpisq = PI_MASS*PI_MASS;
+    // (12)-frame
+    double s3, costheta3, phi3, costheta12, phi12;
+    tin->SetBranchAddress("s3", &s3);
+    tin->SetBranchAddress("costheta3", &costheta3);
+    tin->SetBranchAddress("phi3", &phi3);
+    tin->SetBranchAddress("costheta12", &costheta12);
+    tin->SetBranchAddress("phi12", &phi12);
 
-    // for out mode;
-    MIsobar rho_iso(RHO_MASS, RHO_WIDTH, PI_MASS, PI_MASS, 1, 5.);
-    MIsobar  f2_iso(F2_MASS, F2_WIDTH,  PI_MASS, PI_MASS, 2, 5.);
+    // Phhase space
+    tin->GetEntry(0);
+    double phsp = integrate([s](double _s1)->double{
+        return sqrt(LAMBDA(s, _s1, POW2(PI_MASS))*LAMBDA(_s1, POW2(PI_MASS), POW2(PI_MASS)))/_s1;
+      }, 4*POW2(PI_MASS), POW2(sqrt(s)-PI_MASS)) / (2*M_PI*POW2(8*M_PI)*s);
+    // with isobar
+    double quasi_two_body_phsp = integrate([&, s](double s1)->double{
+        return sqrt(LAMBDA(s, s1, POW2(PI_MASS))*LAMBDA(s1, POW2(PI_MASS), POW2(PI_MASS)))/s1 * 
+          norm(iso[1]->ToneVertex(s1));
+      }, 4*POW2(PI_MASS), POW2(sqrt(s)-PI_MASS)) / (2*M_PI*POW2(8*M_PI)*s);
 
-    double integral = 0;
+    double integral = 0, integral_symm = 0;
     const int Nentries = tin->GetEntries();
     for (int i = 0; i < Nentries; i++) {
       if (i%1000000 == 0 && i != 0) std::cout << "Processing entry " << i << "\n";
       tin->GetEntry(i);
 
-      // introduce quantity which does not depend on subchannels
-      TLorentzVector t_lv = *beam_lv - *reso_lv;
-
-      cd amp_w0 = 0.;
-      cd amp_w1 = 0.;
+      cd amp[2] = {cd(0., 0.), cd(0., 0.)};
       for (uint bose = 0; bose < 2; bose++) {
-        if (bose == 1) {
-          TLorentzVector *tmp = pi1_lv;
-          pi1_lv = pi3_lv;
-          pi3_lv = tmp;
-        }
-
-        TLorentzVector iso_lv = *pi1_lv+*pi2_lv;
-        // calculate important quantities
-        TLorentzVector t_exch_lv = *beam_lv - iso_lv;
-        double t_exch = t_exch_lv.M2();
-        double s_ppi = (*recl_lv+*pi3_lv).M2();
-
-        // calculate z between beam and pion from isobar in the isobar rest frame
-        double s1 = iso_lv.M2();
-        double mbsq = beam_lv->M2();
-        double eb_iso_rf = (s1+mbsq-t_exch)/(2.*sqrt(s1));
-        double epi_iso_rf = (s1+pi1_lv->M2()-pi2_lv->M2())/(2.*sqrt(s1));
-        double pb_iso_rf = sqrt(LAMBDA(s1, mbsq, t_exch)/(4*s1));
-        double ppi_iso_rf = sqrt(LAMBDA(s1, pi1_lv->M2(), pi2_lv->M2())/(4*s1));
-        double z_iso = (eb_iso_rf*epi_iso_rf - (*beam_lv)*(*pi1_lv))/(pb_iso_rf*ppi_iso_rf);
-
-        // other variables
-        double misq = iso_lv.M2();
-
         /*************************************/
+        double sI        = bose?         s3 :         s1;
+        double costhetaI = bose?  costheta3 :  costheta1;
+        double phiI      = bose?       phi3 :       phi1;
+        double costheta  = bose? costheta12 : costheta23;
+        double phi       = bose?      phi12 :      phi23;
+
+        double thetaI = acos(costhetaI);
+        double theta  = acos(costheta);
         // loop over waves
-        integral /*[w]*/ += 0.0;  // should be an anglular function.
+        amp[bose] = Math::ZJMLS(waves[iw].J, waves[iw].M, waves[iw].L, waves[iw].S,
+                           thetaI, phiI, theta, phi) * iso[waves[iw].S]->ToneVertex(sI);
         /*************************************/
       }
+      integral += norm(amp[0]);
+      integral_symm += norm(amp[0]+amp[1])/2.;
     }
     
     f->Close();
 
     // waves[iw];
-    htest.SetBinContent(e+1, integral);
+    htest.SetBinContent(e+1, integral*phsp*POW2(4*M_PI)/Nentries * (8*M_PI));
+    htest2.SetBinContent(e+1, integral_symm*phsp*POW2(4*M_PI)/Nentries * (8*M_PI));
   }
 
   TCanvas c1("c1");
   htest.Draw("hist");
-  c1.Print("/tmp/weghted_plots.pdf", "pdf");
-
-
+  htest2.SetLineColor(kRed); htest2.Draw("same");
+  c1.Print("/tmp/ph.sp.PhoPiS.pdf");
 
   return 0;
+}
+
+
+
+void fill_wavepull(const char* wave_fname, std::vector<wave> *waves) {
+  std::ifstream fin(wave_fname);
+  if (!fin.is_open()) {
+    std::cerr << "Error: can not find the file!\n";
+    return;
+  }
+  std::string line;
+  while (std::getline(fin, line)) {
+    std::istringstream iss(line);
+    wave n;
+    iss >> n.index;
+    iss >> n.title;
+    if (n.title == "FLAT") {
+      n.J = 0; n.M = 0; n.S = -7; n.L = 0;
+      n.neg_refl = true;
+      waves->push_back(n);
+      continue;
+    }
+    iss >> n.J;
+    iss >> n.M;
+    std::string epsilon;
+    iss >> epsilon;
+    if (epsilon == "-") n.neg_refl = true; else n.neg_refl = false;
+    iss >> n.S;
+    iss >> n.L;
+    waves->push_back(n);
+  }  
 }
---
+++ simulations/generate_3pi_angles.cc
@@ -0,0 +1,143 @@
+// Copyright [2017] Misha Mikhasenko
+// A simple program to generate 3 particles phase space decay angles
+
+#include <iostream>
+
+#include "TRandom.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TF2.h"
+// #include "TH1D.h"
+
+#include "constants.h"
+#include "mintegrate.h"
+#include "deflib.h"
+
+double fs1(double *x, double *par) {
+  // double s = par[0];
+  double m1sq = par[0];
+  double m2sq = par[1];
+  double m3sq = par[2];
+  double s1 = x[0];
+  double s = x[1];
+  if ((s1 < POW2(sqrt(m2sq)+sqrt(m3sq)) || (s1 > POW2(sqrt(s)-sqrt(m1sq))))) return 0.;
+  return sqrt(LAMBDA(s, s1, m1sq))/s*sqrt(LAMBDA(s1, m2sq, m3sq))/s1;
+}
+
+int main(int ac, char **av) {
+
+  const char *fout_name = (ac >= 2) ? av[1] : "/tmp/test_generate_3pi_angles.root";
+  const uint nEvents = (ac >= 3) ? atoi(av[2]) : 1e5;
+  const double S_MIN = (ac >= 5)  ? atof(av[3])*atof(av[3]) : (9*PI_MASS*PI_MASS);
+  const double S_MAX = (ac >= 5)  ? atof(av[4])*atof(av[4]) : 9.;
+  const uint seed = (ac >= 6)  ? atoi(av[5]) : 0;
+  std::cout << "Output: " << fout_name << "\n";
+  std::cout << "Number of events: " << nEvents << "\n";
+  std::cout << "M_{3pi} range: [ " << sqrt(S_MIN) << ", " << sqrt(S_MAX) << " ]\n";
+  std::cout << "Seed is " << seed << "\n";
+
+  const double m1sq = POW2(PI_MASS);
+  const double m2sq = POW2(PI_MASS);
+  const double m3sq = POW2(PI_MASS);
+
+  gRandom->SetSeed(seed);
+
+  TF2 tfs1("tfs1", fs1,
+           POW2(2*PI_MASS), POW2(sqrt(S_MAX)-PI_MASS),
+           S_MIN, S_MAX, 3);
+  tfs1.SetParameter(0, m1sq);
+  tfs1.SetParameter(1, m2sq);
+  tfs1.SetParameter(2, m3sq);
+  tfs1.SetNpx(500);
+  tfs1.SetNpy(500);
+
+  TFile fout(fout_name, "RECREATE");
+  TTree tout("angles", "angles");
+  // (23)-frame
+  double s1, costheta1, phi1, costheta23, phi23;
+  tout.Branch("s1", &s1);
+  tout.Branch("costheta1", &costheta1);
+  tout.Branch("phi1", &phi1);
+  tout.Branch("costheta23", &costheta23);
+  tout.Branch("phi23", &phi23);
+
+  // (12)-frame
+  double s3, costheta3, phi3, costheta12, phi12;
+  tout.Branch("s3", &s3);
+  tout.Branch("costheta3", &costheta3);
+  tout.Branch("phi3", &phi3);
+  tout.Branch("costheta12", &costheta12);
+  tout.Branch("phi12", &phi12);
+
+  double s;
+  tout.Branch("s", &s);
+
+  for (uint e = 0; e < nEvents; e++) {
+    // from the function
+    tfs1.GetRandom2(s1, s);  // BRANCH
+
+    costheta1 = 2*gRandom->Rndm()-1.;  // BRANCH
+    phi1 = M_PI*(2*gRandom->Rndm()-1.);  // BRANCH
+    costheta23 = 2*gRandom->Rndm()-1.;  // BRANCH
+    phi23 = M_PI*(2*gRandom->Rndm()-1.);  // BRANCH
+    
+    // calculate s3 in (23) frame
+    s3 = m1sq + m2sq +  // BRANCH
+      // 2*(  E2 * E1 -
+      2*( (s1 + m2sq - m3sq)/(2*sqrt(s1)) * (s-m1sq-s1)/(2*sqrt(s1)) -
+          // |p2|*cos(theta23) * (-|p1|)
+          sqrt(LAMBDA(s1, m2sq, m3sq)/(4*s1))*costheta23 * (-sqrt(LAMBDA(s, m1sq, s1)/(4*s1))) );
+    if (s3 != s3) continue;  // nan check
+
+    // caluclate p3 in (23) frame
+    double p23bu = sqrt(LAMBDA(s1, m2sq, m3sq)/(4*s1));
+    double p3_in23[] = {(s1 + m3sq - m2sq)/sqrt(4*s1),
+                        -p23bu*sqrt(1-POW2(costheta23))*cos(phi23),
+                        -p23bu*sqrt(1-POW2(costheta23))*sin(phi23),
+                        -p23bu*costheta23};
+    double gamma1 = (s + s1 - m1sq)/sqrt(4*s*s1);
+    double beta1 = sqrt(1.-1./POW2(gamma1));
+    double p3_b[] = {gamma1*(p3_in23[0]+beta1*p3_in23[3]),
+                     p3_in23[1],
+                     p3_in23[2],
+                     gamma1*(beta1*p3_in23[0]+p3_in23[3])};
+    double p3_rot[4];
+    { double ct = costheta1, st = sqrt(1.-POW2(costheta1)), cp = cos(phi1), sp = sin(phi1);
+      // Rz(phi1) * Ry(theta1) * p3_boost
+      p3_rot[0] = p3_b[0];
+      p3_rot[1] = cp*ct* p3_b[1] + (-sp)* p3_b[2] + cp*st* p3_b[3];
+      p3_rot[2] = sp*ct* p3_b[1] +   cp * p3_b[2] + sp*st* p3_b[3];
+      p3_rot[3] =  - st* p3_b[1] +    0 * p3_b[2] +    ct* p3_b[3];
+    }
+    // calculate Omega12
+    // BRANCH
+    costheta3 = - p3_rot[3]/sqrt(POW2(p3_rot[1])+POW2(p3_rot[2])+POW2(p3_rot[3]));
+    phi3 = atan2(-p3_rot[2], -p3_rot[1]);  // BRANCH
+
+    // BRANCH
+    costheta12 = (s1 - m2sq - m3sq - 2* (s3+m2sq-m1sq)/(2*sqrt(s3)) * (s-m3sq-s3)/(2*sqrt(s3))) /
+      (2 * sqrt(LAMBDA(s3, m1sq, m2sq)/(4*s3)) * sqrt(LAMBDA(s, m3sq, s3)/(4*s3)) );
+    if (costheta12 != costheta12) continue;  // nan check
+
+    double n1[] = {0.,
+                   -sqrt(1.-POW2(costheta1))*cos(phi1),
+                   -sqrt(1.-POW2(costheta1))*sin(phi1),
+                   -costheta1};
+    // Ry(-theta12) * Rz(-phi12) * n1;
+    double n1_rot[4];
+    { double ct = costheta3, st = sqrt(1.-POW2(costheta3)), cp = cos(phi3), sp = sin(phi3);
+      // Rz(phi23) * Ry(theta23) * p3_boost
+      n1_rot[0] = n1[0];
+      n1_rot[1] = cp*ct* n1[1] + sp*ct* n1[2] + (-st)* n1[3];
+      n1_rot[2] = (-sp)* n1[1] +   cp * n1[2] +    0 * n1[3];
+      n1_rot[3] = cp*st* n1[1] + sp*st* n1[2] +    ct* n1[3];
+    }
+    phi12 = atan2(n1_rot[2], n1_rot[1]);  // BRANCH
+
+    tout.Fill();
+  }
+  tout.Write();
+  fout.Close();
+  
+  return 0;
+}
