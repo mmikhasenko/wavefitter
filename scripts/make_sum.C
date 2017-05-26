62a37daed5da8373b79bfc4010622363ffac4fd3
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-05-26 18:26:50 +0200

    Improved the program to plot projections. Now it selects sets and plot it

--- scripts/make_sum.C
+++ scripts/make_sum.C
@@ -14,41 +14,81 @@
 
 typedef std::complex<double> cd;
 
+#define NAME_PATT "h2int%d"
+
+void find_numbers_of_hists_which_much_the_pattern(const char *name_patt,
+                                                  std::vector<uint> from,
+                                                  const char* match_pattern,
+                                                  std::vector<uint> *result);
+
 TH1D *make_hsum(std::vector<TH1D*> vec);
 
 TCanvas *plot_analytical_deck_components(const char *fin_name) {
 
+        TFile *fin = new TFile(fin_name);
+
         std::vector< std::pair<std::string, std::vector<uint>> > waves;
-        { std::vector<uint> v = {  1, 213}; waves.push_back(std::make_pair("The sum;M_{3#pi}", v)); }
-        { std::vector<uint> v = {  4,  24}; waves.push_back(std::make_pair("1^{?+}", v)); }
-        { std::vector<uint> v = {  1,   3}; waves.push_back(std::make_pair("0^{?+}", v)); }
-        { std::vector<uint> v = { 25,  69}; waves.push_back(std::make_pair("2^{?+}", v)); }
-        { std::vector<uint> v = { 70, 132}; waves.push_back(std::make_pair("3^{?+}", v)); }
-        { std::vector<uint> v = {133, 213}; waves.push_back(std::make_pair("4^{?+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("1^{++}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("0^{-+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("2^{-+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("1^{-+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("2^{++}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("3^{-+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("3^{++}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("4^{-+}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("4^{++}", v)); }
-        // { std::vector<uint> v = {}; waves.push_back(std::make_pair("#epsilon = (-)", v)); }
+        // all
+        { std::vector<uint> v(213);
+          for (uint i = 0; i < v.size(); i++) v[i] = i+1;
+          waves.push_back(std::make_pair("The sum, J < 5;M_{3#pi}", v));}
+        // positive reflectivity
+        std::vector<uint> vpos;
+        find_numbers_of_hists_which_much_the_pattern(NAME_PATT, waves[0].second, "+(S", &vpos);
+        std::cout << "Found " << vpos.size() << " hists with positive refl.\n";
+        // 1++
+        { std::vector<uint> v = {};
+          find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "1++", &v);
+          waves.push_back(std::make_pair("1^{++}", v));}
+        // 0-+
+        { std::vector<uint> v = {};
+          find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "0-+", &v);
+          waves.push_back(std::make_pair("0^{-+}", v));}
+        // 2-+
+        { std::vector<uint> v = {};
+          find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "2-+", &v);
+          waves.push_back(std::make_pair("2^{-+}", v));}
+        // neg reflectivity
+        std::vector<uint> vneg;
+        find_numbers_of_hists_which_much_the_pattern(NAME_PATT, waves[0].second, "-(S", &vneg);
+        std::cout << "Found " << vneg.size() << " hists with negative refl.\n";
+        waves.push_back(std::make_pair("#epsilon = (-)", vneg));
+        // 1-+
+        { std::vector<uint> v = {};
+          find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "1-+", &v);
+          waves.push_back(std::make_pair("1^{-+}", v));}
+        // 2++
+        { std::vector<uint> v = {};
+          find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "2++", &v);
+          waves.push_back(std::make_pair("2^{++}", v));}
+        // // 3++
+        // { std::vector<uint> v = {};
+        //   find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "3++", &v);
+        //   waves.push_back(std::make_pair("3^{++}", v));}
+        // // 4-+
+        // { std::vector<uint> v = {};
+        //   find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "4-+", &v);
+        //   waves.push_back(std::make_pair("4^{-+}", v));}
+        // // 3-+
+        // { std::vector<uint> v = {};
+        //   find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "3-+", &v);
+        //   waves.push_back(std::make_pair("3^{-+}", v));}
+        // // 4++
+        // { std::vector<uint> v = {};
+        //   find_numbers_of_hists_which_much_the_pattern(NAME_PATT, vpos, "4++", &v);
+        //   waves.push_back(std::make_pair("4^{++}", v));}
+
 
         TCanvas *c1 = new TCanvas("c1", "title", 1000, 1000);
 
-        TFile *fin = new TFile(fin_name);
         THStack *hs = new THStack("hs", "Intensities for the coherent sum of projections;M_{3#pi}");
         for (auto & wi : waves) {
                 std::vector<TH1D*> comp;
-                for (uint w = wi.second[0]; w <= wi.second[1]; w++) {
-                        TH2D *h2; gDirectory->GetObject(TString::Format("h2int%d", w), h2);
+                for (uint & w : wi.second) {
+                        TH2D *h2; gDirectory->GetObject(TString::Format(NAME_PATT, w), h2);
                         if (!h2) {
                                 std::cerr << "Error: hist " << w << " is not found!\n";
                                 return 0;
-                        } else {
-                                std::cout << "Success! " << h2->GetTitle() << "\n";
                         }
                         TH1D *h1 = h2->ProjectionX();
                         comp.push_back(h1);
@@ -66,7 +106,7 @@ TCanvas *plot_analytical_deck_components(const char *fin_name) {
 }
 
 TH1D *make_hsum(std::vector<TH1D*> vec) {
-        for (auto & h : vec) std::cout << h->GetTitle() << " " << h->GetBinContent(50) << "\n";
+        // for (auto & h : vec) std::cout << h->GetTitle() << " " << h->GetBinContent(50) << "\n";
         const uint Nbins = 100;
         TH1D *hsum = new TH1D("hsum", "title", Nbins, 0.5, 2.5);
         for (uint b = 0; b < Nbins; b++) {
@@ -76,4 +116,18 @@ TH1D *make_hsum(std::vector<TH1D*> vec) {
                 }
         }
         return hsum;
+}
+
+void find_numbers_of_hists_which_much_the_pattern(const char *name_patt,
+                                                  std::vector<uint> from, const char* match_pattern,
+                                                  std::vector<uint> *result) {
+        for (uint & i : from) {
+                TH2D *h; gDirectory->GetObject(TString::Format(name_patt, i), h);
+                if (!h) {
+                        std::cerr << "Error: hist with the name " << TString::Format(name_patt, i) << " not found\n";
+                        return;
+                }
+                std::string title(h->GetTitle());
+                if (title.find(match_pattern) != std::string::npos) result->push_back(i);
+        }
 }
