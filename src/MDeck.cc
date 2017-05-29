5991190625c22c718ad88faaca9170551d76fbd7
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-05-29 13:01:50 +0200

    To previous commit

--- src/MDeck.cc
+++ src/MDeck.cc
@@ -29,10 +29,10 @@ cd MDeck::getAmplitude(double costheta, double phi,
   isobarT[2] = [&](double s) -> cd { return f2.T(s) * sqrt(2.)/3. * BrF2pipi; };
 
   // Blatt-Weisskopf factors
-  double qsq = LAMBDA(mS1sq, m2sq, m3sq)/(4*mS1sq);  // because m2 and m3 are used in the constructor
-  // double t1 = MAscoli::tPionIsobar(costheta, mS1sq, wsq, t, mAsq, m1sq);
-  double qsq_left = LAMBDA(mS1sq, mtRsq, mAsq)/(4*mS1sq);  // that is what one should use
-  double ratio = (1./(RS1*RS1*qsq)+1.)/(1./(RS1*RS1*qsq_left)+1.);  // correction
+  // double qsq = LAMBDA(mS1sq, m2sq, m3sq)/(4*mS1sq);  // because m2 and m3 are used in the constructor
+  //// double t1 = MAscoli::tPionIsobar(costheta, mS1sq, wsq, t, mAsq, m1sq);
+  // double qsq_left = LAMBDA(mS1sq, mtRsq, mAsq)/(4*mS1sq);  // that is what one should use
+  // double ratio = (1./(RS1*RS1*qsq)+1.)/(1./(RS1*RS1*qsq_left)+1.);  // correction
   // loop over spin, helicity
   cd amp = 0.0;
   for (uint S = 0; S <= 2; S++) {
@@ -52,7 +52,7 @@ cd MDeck::getAmplitude(double costheta, double phi,
       std::cerr << "Error<MDeck::getAmplitude> : imag = " << imag(deck_fixed_S_isobar_reduced) << " > 1e-8!!\n";
       return 0.0;
     }
-    amp += (2.*S+1.) * deck_fixed_S_isobar_reduced * isobarT[S](mS1sq) * pow(ratio, S/2.);
+    amp += (2.*S+1.) * deck_fixed_S_isobar_reduced * isobarT[S](mS1sq);  // * pow(ratio, S/2.);
   }
   return amp;
 }
