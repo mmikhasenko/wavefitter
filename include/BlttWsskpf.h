c4a219d1428ebe0b4167475f86d4cf3f1b7ca6bd
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-08-24 02:10:56 +0300

    converted integers to doubles, fixed complex functions in MIsobars

--- include/BlttWsskpf.h
+++ include/BlttWsskpf.h
@@ -8,13 +8,13 @@ template <typename Type> class FFModel {
 };
 
 template <typename Type> std::vector<std::function<Type(Type)> > FFModel<Type>::BlttWskpf = {
-  [](Type z)->Type{return 1;},
-  [](Type z)->Type{return 2*z/(z+1);},
-  [](Type z)->Type{return 13*z*z/(z*z+3*z+9);},
-  [](Type z)->Type{return 277*z*z*z/(z*z*z+6*z*z+45*z+225);},
-  [](Type z)->Type{return 12746*z*z*z*z/(z*z*z*z+10*z*z*z+135*z*z+1575*z+11025);},
-  [](Type z)->Type{return 998881*z*z*z*z*z/(z*z*z*z*z+15*z*z*z*z+315*z*z*z+6300*z*z+99225*z+893025);},
-  [](Type z)->Type{return 118394977*z*z*z*z*z*z/(z*z*z*z*z*z+21*z*z*z*z*z+630*z*z*z*z+18900*z*z*z+496125*z*z+9823275*z+108056025);}
+  [](Type z)->Type{return 1.;},
+  [](Type z)->Type{return 2.*z/(z+1.);},
+  [](Type z)->Type{return 13.*z*z/(z*z+3.*z+9.);},
+  [](Type z)->Type{return 277.*z*z*z/(z*z*z+6.*z*z+45.*z+225.);},
+  [](Type z)->Type{return 12746.*z*z*z*z/(z*z*z*z+10.*z*z*z+135.*z*z+1575.*z+11025.);},
+  [](Type z)->Type{return 998881.*z*z*z*z*z/(z*z*z*z*z+15.*z*z*z*z+315.*z*z*z+6300.*z*z+99225.*z+893025.);},
+  [](Type z)->Type{return 118394977.*z*z*z*z*z*z/(z*z*z*z*z*z+21.*z*z*z*z*z+630.*z*z*z*z+18900.*z*z*z+496125.*z*z+9823275.*z+108056025.);}
 };
 
 typedef FFModel<double> FFMod;
--- src/MIsobar.cc
+++ src/MIsobar.cc
@@ -35,15 +35,15 @@ double MIsobar::U(double s) const {
   double G = G0 / RHO(M*M, m1*m1, m2*m2)  *  RHO(s, m1*m1, m2*m2);
   const double p_2 = LAMBDA(s, m1*m1, m2*m2)/(4.*s);
   const double p0_2 = LAMBDA(M*M, m1*m1, m2*m2)/(4.*M*M);
-  G *=  pow(p_2/p0_2 * (1.+R*R*p0_2)/(1.+R*R*p_2), L);
+  double BltWskp = FFMod::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
+  G *=  BltWskp;
   return 2.0*M*G/(POW2(M*M-s)+POW2(M*G)) / intU;
 }
 
 cd MIsobar::U(cd s) const {
   const cd p_2 = LAMBDA(s, m1*m1, m2*m2)/(4.*s);
   const double p0_2 = LAMBDA(M*M, m1*m1, m2*m2)/(4.*M*M);
-  // cd BltWskp = pow(p_2/p0_2 * (1.+R*R*p0_2)/(1.+R*R*p_2), L);
-  double BltWskp = FFMod::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
+  cd BltWskp = FFMoc::BlttWskpf[L](R*R*p_2) / FFMoc::BlttWskpf[L](R*R*p0_2);
 
   cd G = G0/RHO(M*M, m1*m1, m2*m2)  *  RHO(s, m1*m1, m2*m2) * BltWskp;
   return 2.0*M*G/(POW2(M*M-s)+POW2(M*G)) / intU;
@@ -53,32 +53,29 @@ cd MIsobar::ToneVertex(double s) const {
   if (s < POW2(m1+m2)) return 0.0;
   const double p_2 = LAMBDA(s, m1*m1, m2*m2)/(4.*s);
   const double p0_2 = LAMBDA(M*M, m1*m1, m2*m2)/(4.*M*M);
-  // double BltWskp = pow(p_2/p0_2 * (1.+R*R*p0_2)/(1.+R*R*p_2), L);
   double BltWskp = FFMod::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
   
   double G = G0/RHO(M*M, m1*m1, m2*m2)  *  RHO(s, m1*m1, m2*m2) * BltWskp;
   double gsq = 2*M*G0/RHO(M*M, m1*m1, m2*m2) * BltWskp;
-  return sqrt(gsq/intU)/(M*M-s-cd(0., M)*G);  // / intU
+  return sqrt(gsq/intU)/(M*M-s-cd(0., M)*G);
 }
 
 cd MIsobar::T(cd     s) const {
   const cd p_2 = LAMBDA(s, m1*m1, m2*m2)/(4.*s);
   const double p0_2 = LAMBDA(M*M, m1*m1, m2*m2)/(4.*M*M);
-  // cd BltWskp = pow(p_2/p0_2 * (1.+R*R*p0_2)/(1.+R*R*p_2), L);
-  double BltWskp = FFMod::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
+  cd BltWskp = FFMoc::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
 
   cd G = G0/RHO(M*M, m1*m1, m2*m2)  *  RHO(s, m1*m1, m2*m2) * BltWskp;
   cd gsq = 2*M*G0/RHO(M*M, m1*m1, m2*m2) * BltWskp;
-  return gsq/(M*M-s-cd(0., M)*G);  // / intU
+  return gsq/(M*M-s-cd(0., M)*G);
 }
 
 cd MIsobar::T(double s) const {
   const double p_2 = LAMBDA(s, m1*m1, m2*m2)/(4.*s);
   const double p0_2 = LAMBDA(M*M, m1*m1, m2*m2)/(4.*M*M);
-  // double BltWskp = pow(p_2/p0_2 * (1.+R*R*p0_2)/(1.+R*R*p_2), L);
   double BltWskp = FFMod::BlttWskpf[L](R*R*p_2) / FFMod::BlttWskpf[L](R*R*p0_2);
 
   double G = G0/RHO(M*M, m1*m1, m2*m2)  *  RHO(s, m1*m1, m2*m2) * BltWskp;
   double gsq = 2*M*G0/RHO(M*M, m1*m1, m2*m2) * BltWskp;
-  return gsq/(M*M-s-cd(0., M)*G);  // / intU;
+  return gsq/(M*M-s-cd(0., M)*G);
 }
