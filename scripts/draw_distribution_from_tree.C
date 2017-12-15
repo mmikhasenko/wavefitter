68efc039690c7a6e0dcaec326fb1f0e932ab6f42
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2017-12-15 11:18:47 +0100

    Added mode checks to a script to draw distribution from a tree using TTreePlayer

--- scripts/draw_distribution_from_tree.C
+++ scripts/draw_distribution_from_tree.C
@@ -21,25 +21,27 @@ void draw_distribution_from_tree(const char *request,
                                  const char *fin_templ,
                                  uint bin = 1,
                                  const std::vector<uint> _v = {},
-                                 bool SYMM = false);
+                                 bool SYMM = false,
+                                 const char *cut = "s>0");
 
 void draw_distribution_from_tree(const char *request,
                                  const char *fproj_name,
                                  const char *fin_templ,
                                  uint bin,
                                  const std::vector<uint> _v,
-                                 bool SYMM) {
+                                 bool SYMM,
+                                 const char *cut) {
   std::vector<uint> v(_v);
   if (v.size() == 0) {
-    v.resize(88-1); for (uint i = 0; i < 88-1; i++) v[i] = i+2;
+    v.resize(88); for (uint i = 0; i < 88; i++) v[i] = i+1;
   }
   const uint Nwaves = v.size();
 
   TFile *fproj = new TFile(fproj_name);
   double bamp[Nwaves][2];
   for (uint w = 0; w < v.size(); w++) {
-    TH1D *hr = 0; gDirectory->GetObject(TString::Format("hr%d", v[w]), hr); if (!hr) return;
-    TH1D *hi = 0; gDirectory->GetObject(TString::Format("hi%d", v[w]), hi); if (!hi) return;
+    TH1D *hr = 0; gDirectory->GetObject(TString::Format("hr%d", v[w]), hr); if (!hr) {std::cerr << "No hr" << v[w] << "\n!"; return;}
+    TH1D *hi = 0; gDirectory->GetObject(TString::Format("hi%d", v[w]), hi); if (!hi) {std::cerr << "No hi" << v[w] << "\n!"; return;}
     bamp[w][0] = hr->GetBinContent(bin);
     bamp[w][1] = hi->GetBinContent(bin);
   }
@@ -47,8 +49,8 @@ void draw_distribution_from_tree(const char *request,
 
   // work on tree
   TFile *fin = new TFile(TString::Format(fin_templ, bin-1));
-  TTree *tin = 0; gDirectory->GetObject("angles", tin); if (!tin) return;
-  TString weight("(");
+  TTree *tin = 0; gDirectory->GetObject("angles", tin); if (!tin) {std::cerr << "No tree!\n"; return;}
+  TString weight(TString::Format("(%s)*(", cut));
   ROOT::v5::TFormula::SetMaxima(100000, 100000, 100000);
   //*** real part
   if (SYMM) {
@@ -64,7 +66,7 @@ void draw_distribution_from_tree(const char *request,
   tin->Draw(request, weight);
   // std::cout << "first request: "<< request << "\n";
   //**** imag part
-  weight = "(";
+  weight = TString::Format("(%s)*(", cut);
   if (SYMM) {
     for (uint w = 0; w < v.size(); w++) weight += TString::Format("(amp%d_frame1_real+amp%d_frame3_real)*(%f)+", v[w], v[w], bamp[w][1]);
     for (uint w = 0; w < v.size(); w++) weight += TString::Format("(amp%d_frame1_imag+amp%d_frame3_imag)*(%f)+", v[w], v[w], bamp[w][0]);
